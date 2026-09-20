9118171f3af302af6ba7c60d403c00028b2e1d1d
Ana Cabrera-Bello <anamcabrerabello@gmail.com>
2026-09-20 16:02:24 -0400

    small bug fix

--- src/javascript/graphics.js
+++ src/javascript/graphics.js
@@ -559,7 +559,7 @@ export async function loadExpensesVsBudgetsOverTimePerCategory()
         average = average / divideBy;
 
 
-        expensesVsBudgetsBarChart = new Chart(chart, {
+        expensesVsBudgetsBarChartPerCategory = new Chart(chart, {
             type: "bar",
             data:
             {
